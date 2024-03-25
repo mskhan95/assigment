91c3da313088e0ed6fb50e1659433007ec8c88b1
mskhan1995 <shabhankhan123456@gmail.com>
2024-03-25 06:08:27 +0530

    Private Routes Changed

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Login from "./Login";
+import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
   const token = JSON.parse(localStorage.getItem("token"));
   if (!token) {
-    return <Login />;
+    return <Navigate to={"/"} />;
   }
   return children;
 };
