97704de088d46a8928b409f429e00f3fdf43e4fe
mskhan1995 <shabhankhan123456@gmail.com>
2024-03-25 07:24:27 +0530

    Issue Solved

--- src/Redux/Product/product.action.js
+++ src/Redux/Product/product.action.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 import * as types from "../actionType";
 
-const token = JSON.parse(localStorage.getItem("token"));
-
 export const getProducts = () => async (dispatch) => {
   dispatch({ type: types.GET_PRODUCT_PENDING });
+  const token = await JSON.parse(localStorage.getItem("token"));
   try {
     let res = await axios.get(
       `https://staging.fastor.in/v1/m/restaurant?city_id=118`,
